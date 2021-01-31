43f8f37e54cc682bef3bfbdce2ff089e86e4d5bb
relvaner <david4bauer@googlemail.com>
2021-01-31 18:49:15 +0100

    actor4j-data-mongo: Update for Logging

--- actor4j-data-mongo/src/test/java/io/actor4j/core/data/access/features/PersistentCacheFeature.java
+++ actor4j-data-mongo/src/test/java/io/actor4j/core/data/access/features/PersistentCacheFeature.java
@@ -37,7 +37,7 @@ import io.actor4j.core.data.access.VolatileDataAccessObject;
 import io.actor4j.core.data.access.mongo.MongoDataAccessActor;
 import io.actor4j.core.data.access.utils.PersistentActorCacheManager;
 
-import static io.actor4j.core.logging.user.ActorLogger.logger;
+import static io.actor4j.core.logging.ActorLogger.*;
 import static org.junit.Assert.*;
 
 import java.util.Timer;
@@ -105,7 +105,7 @@ public class PersistentCacheFeature {
 					VolatileDataAccessObject<String, TestObject> payload = ((VolatileDataAccessObject<String, TestObject>)msg.value);
 					if (payload.value!=null) {
 						assertEquals(values[i], payload.value.value);
-						logger().debug(payload.value.value);
+						logger().log(DEBUG, payload.value.value);
 						if (i<keys.length-1)
 							i++;
 						testDone.countDown();
@@ -175,7 +175,7 @@ public class PersistentCacheFeature {
 						assertEquals(keys[i], pair.a);
 						assertEquals(keys[i], pair.b.key);
 						assertEquals(values[i], pair.b.value);
-						logger().debug(pair.b.value);
+						logger().log(DEBUG, pair.b.value);
 						if (i<keys.length-1)
 							i++;
 						testDone.countDown();
