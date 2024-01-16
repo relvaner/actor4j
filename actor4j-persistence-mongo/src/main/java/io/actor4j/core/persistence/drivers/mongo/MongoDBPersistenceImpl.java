0d6c5554249166ffe62ec48674de6e341a3eb424
relvaner <david4bauer@googlemail.com>
2024-01-16 20:30:20 +0100

    -persistence-mongo: Minor update

--- actor4j-persistence-mongo/src/main/java/io/actor4j/core/persistence/drivers/mongo/MongoDBPersistenceImpl.java
+++ actor4j-persistence-mongo/src/main/java/io/actor4j/core/persistence/drivers/mongo/MongoDBPersistenceImpl.java
@@ -44,7 +44,7 @@ import static io.actor4j.core.runtime.protocols.ActorProtocolTag.INTERNAL_PERSIS
 import static io.actor4j.core.runtime.protocols.ActorProtocolTag.INTERNAL_PERSISTENCE_SUCCESS;
 
 public class MongoDBPersistenceImpl extends PersistenceImpl {
-	private static final ObjectMapper objectMapper;
+	private static final ObjectMapper objectMapper = ObjectMapper.create();
 	
 	protected MongoDatabase database;
 	protected MongoCollection<Document> events;
@@ -52,10 +52,6 @@ public class MongoDBPersistenceImpl extends PersistenceImpl {
 	
 	protected long lastTimeStamp;
 	protected int indexIfEqualTimeStamp;
-	
-	static {
-		objectMapper = ObjectMapper.create();
-	}
 	
 	public MongoDBPersistenceImpl(ActorSystem parent, PersistenceDriver driver) {
 		super(parent, driver);
