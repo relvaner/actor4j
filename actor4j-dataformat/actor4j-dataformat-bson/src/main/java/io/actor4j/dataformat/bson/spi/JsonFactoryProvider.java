76ba6af3764edaf6018a124bc2f8809f0fe57715
relvaner <david4bauer@googlemail.com>
2024-06-22 23:50:42 +0200

    -dataformat-bson: Fix

--- actor4j-dataformat/actor4j-dataformat-bson/src/main/java/io/actor4j/dataformat/bson/spi/JsonFactoryProvider.java
+++ actor4j-dataformat/actor4j-dataformat-bson/src/main/java/io/actor4j/dataformat/bson/spi/JsonFactoryProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.actor4j.dataformat.spi;
+package io.actor4j.dataformat.bson.spi;
 
 import java.util.List;
 
